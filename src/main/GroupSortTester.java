1ed0525b74f2e0cc9109d9e0f2b2877a8ddde6a7
Chad <chadd_84@yahoo.com>
2020-02-14 19:22:26 -0800

    Added comments

--- src/main/GroupSortTester.java
+++ src/main/GroupSortTester.java
@@ -1,45 +1,94 @@
+/*
+ * Chad Drennan
+ * 2/14/2020
+ * GroupSortTester.java
+ * This file contains the GroupSortTester class which tests the GroupSort class
+ */
+
 package main;
 
-import sortingAlgorithm.GroupSort;
+import sortingalgorithm.GroupSort;
 
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Tests the GroupSort class for bugs
+ * @author Chad Drennan
+ * @version 1.0
+ */
 public class GroupSortTester {
 
-    public static final int ARRAY_SIZE = 15;
-    public static final int RAND_UPPER_BOUND = 30;
+    // Array size to create for testing
+    public static final int ARRAY_SIZE = 20;
 
+    // Inclusive upper boundary for random int range
+    public static final int RAND_UPPER_BOUND = 100;
+
+
+    /**
+     * Starts the program
+     * @param args not used
+     */
     public static void main(String[] args) {
-        System.out.println("Group Sorter");
-        System.out.println("************\n");
-        System.out.println("How many groups would you like to sort with?");
 
-        Scanner console = new Scanner(System.in);
-        int groupQty = console.nextInt();
+        // Create test array and display data to the user
+        int groupQty = introAndGetGroupQty();
+        int[] array = createRandomArray();
+        displayDataBeforeSort(groupQty, array);
 
+        // Sort the array
+        GroupSort sorter = new GroupSort();
+        sorter.sort(array, groupQty);
+
+        // Display sorted data
+        displayDataAfterSort(array);
+    }
+
+
+    // Creates random array for testing
+    private static int[] createRandomArray() {
         Random rand = new Random();
         int[] array = new int[ARRAY_SIZE];
 
         for (int i = 0; i < array.length; i++) {
             array[i] = rand.nextInt(RAND_UPPER_BOUND) + 1;
         }
+        return array;
+    }
 
+
+    // Displays test array data after it has been sorted and checks for inversions
+    private static void displayDataAfterSort(int[] array) {
+        System.out.println("Sorted: " + Arrays.toString(array));
+        System.out.println("Detected inversions? " + hasInversions(array));
+    }
+
+
+    // Displays test array data before it has been sorted
+    private static void displayDataBeforeSort(int groupQty, int[] array) {
         System.out.println("Generated an array of size " + ARRAY_SIZE
                 + " with a range of [1, " + RAND_UPPER_BOUND + "]");
         System.out.println("Original: " + Arrays.toString(array));
         System.out.println();
         System.out.println("Sorting with " + groupQty + " groups");
-
-        GroupSort sorter = new GroupSort();
-        sorter.sort(array, groupQty);
-
-        System.out.println("Sorted: " + Arrays.toString(array));
-        System.out.println("Detected inversions? " + hasInversions(array));
     }
 
-    public static boolean hasInversions(int[] array) {
+
+    // Displays title of program and prompts user for group quantity
+    private static int introAndGetGroupQty() {
+        System.out.println("Group Sorter");
+        System.out.println("************\n");
+        System.out.println("How many groups would you like to sort with?");
+
+        Scanner console = new Scanner(System.in);
+        return console.nextInt();
+    }
+
+
+    // Determines if an array has atleast one inversion.
+    private static boolean hasInversions(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
                 return true;
---
+++ src/sortingalgorithm/GroupSort.java
@@ -0,0 +1,191 @@
+/*
+ * Chad Drennan
+ * 2/14/2020
+ * GroupSort.java
+ * This file contains the GroupSort class which uses the bucket sorting algorithm to sort an array of integers
+ */
+
+package sortingalgorithm;
+
+/**
+ * This class sorts an array of ints using the bucket sorting algorithm.
+ * @author Chad Drennan
+ * @version 1.0
+ */
+public class GroupSort {
+
+    // To make upper bounds inclusive and counter rounding errors
+    public static final double THRESHOLD_ADJUSTMENT = .000001;
+
+    /**
+     * Sorts an array of integers in ascending order using groups.
+     * Must supply groupQty of 1 or greater
+     * @param sourceArray the array to sort
+     * @param groupQty amaunt of groups to sort with
+     * @throws IllegalArgumentException if groupQty is less than 1
+     */
+    public void sort(int[] sourceArray, int groupQty) {
+
+        // Preconditions
+        if (sourceArray.length < 2) {
+            return;
+        }
+
+        if (groupQty < 1) {
+            throw new IllegalArgumentException("Group quantity must be greater than 1");
+        }
+
+        int min = sourceArray[0];
+        int max = sourceArray[0];
+
+        // Find max and min
+        for (int currInt : sourceArray) {
+            if (currInt < min) {
+                min = currInt;
+            }
+            if (currInt > max) {
+                max = currInt;
+            }
+        }
+        System.out.println("Min/max: [" + min + ", " + max + "]");
+
+        // Range of numbers divided by groupQty is group size
+        double groupSize = (double)(max - min) / groupQty;
+
+        // Group the data
+        Node[] groups = createGroupsAndAddItems(sourceArray, groupQty, min, groupSize);
+        displayGroupData(groupQty, min, groupSize);
+
+        // Sort the source array
+        putSortedItemsIntoSourceArray(sourceArray, min, groupSize, groups);
+    }
+
+
+    // Helper method to sort method
+    private void putSortedItemsIntoSourceArray(int[] sourceArray, int min, double groupSize, Node[] groups) {
+        int srcIndex = 0;
+        int lowerBound = min;
+
+        for (int i = 0; i < groups.length; i++) {
+            lowerBound = displayGroupRange(min, groupSize, lowerBound, i);
+            srcIndex = putGroupIntoSourceArray(sourceArray, groups[i], srcIndex);
+        }
+        System.out.println();
+    }
+
+
+    // Helper method to putSortedItemsIntoSourceArray method
+    private int displayGroupRange(int min, double groupSize, int lowerBound, int i) {
+        int upperBound = (int)((i + 1) * groupSize + min);
+        System.out.print(i + "[" + lowerBound + ", " + upperBound + "]: ");
+        lowerBound = upperBound + 1;
+        return lowerBound;
+    }
+
+
+    // Helper method to putGroupIntoSourceArray method
+    private int putGroupIntoSourceArray(int[] sourceArray, Node group, int srcIndex) {
+        Node currNode = group;
+
+        // Fencepost
+        if (currNode != null) {
+            System.out.print(currNode.data);
+
+            sourceArray[srcIndex] = currNode.data;
+            srcIndex++;
+            currNode = currNode.next;
+        }
+        else {
+            System.out.print("null");
+        }
+
+        // Add each node from the group to the array
+        while (currNode != null) {
+            System.out.print(" -> " + currNode.data);
+
+            sourceArray[srcIndex] = currNode.data;
+            srcIndex++;
+            currNode = currNode.next;
+        }
+        System.out.println();
+        return srcIndex;
+    }
+
+
+    // Helper method to sort method
+    private Node[] createGroupsAndAddItems(int[] sourceArray, int groupQty, int min, double groupSize) {
+        Node[] groups = new Node[groupQty];
+
+        for (int currInt : sourceArray) {
+            int groupIndex = (int)((currInt - min) / groupSize - THRESHOLD_ADJUSTMENT);
+            addItemToGroup(currInt, groupIndex, groups);
+        }
+        return groups;
+    }
+
+
+    // Helper method to sort method
+    private void displayGroupData(int groupQty, int min, double groupSize) {
+        System.out.println("Group size: " + groupSize);
+
+        // Fencepost
+        System.out.print("Group thresholds: [" + (min + (int)groupSize));
+
+        for (int i = 2; i <= groupQty; i++) {
+            System.out.print(", " + (int)(i * groupSize + min));
+        }
+        System.out.println("]\n");
+    }
+
+
+    // Helper method to createGroupsAndAddItems method
+    private void addItemToGroup(int currInt, int groupIndex, Node[] groups) {
+        Node curr = groups[groupIndex];
+
+        // Put node in front if first one or smallest
+        if (curr == null || curr.data > currInt) {
+            groups[groupIndex] = new Node(currInt, curr);
+            return;
+        }
+
+        // Place item before first item with higher value or at the end
+        while (curr.next != null && curr.next.data < currInt) {
+            curr = curr.next;
+        }
+        curr.next = new Node(currInt, curr.next);
+    }
+
+
+    /**
+     * Represents a singly linked node to other nodes and containing integer data
+     * @author Chad Drennan
+     * @version 1.0
+     */
+    private class Node {
+        // Holds integer values
+        private int data;
+
+        // The node this node links to
+        private Node next;
+
+
+        /**
+         * 1 arg constructor
+         * @param data the integer data this node contains
+         */
+        private Node(int data) {
+            this(data, null);
+        }
+
+
+        /**
+         * 2 arg constructor
+         * @param data integer data for this node
+         * @param next node this node links to
+         */
+        private Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
